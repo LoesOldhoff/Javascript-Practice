e8e400744c2f1043f47c5aea1214c56f3556c480
Loes Oldhoff <loesoldhoff@hotmail.com>
2023-03-03 18:51:33 +0100

    worked on flappy obstacles

--- Flappy/obstacles.js
+++ Flappy/obstacles.js
@@ -1,1 +1,27 @@
 const obstacleArray = [];
+
+class Obstacle(){
+    constructor(){
+        this.top = (Math.random() * canvas.height/3) + 20;
+        this.bottom = (Math.random() * canvas.height/3) + 20;
+        this.width = 20;
+        this.x = canvas.width;
+        this.color = 'hsl(' + hue + '100%, 50%)';        
+    }
+    draw(){
+        ctx.FillStyle(this.color);
+        ctx.FillRect(this.x, 0, this.width, this.top);
+        ctx.FillRect(this.x, canvas.height - this.bottom, this.width, this.bottom);
+    }
+    update(){
+        this.x -= gamespeed;
+        this.draw();
+    }
+}
+
+function handleObstacle(){
+    if (frame%50 === 0){
+        obstacleArray.unshift(new Obstacle);
+        
+    }
+}
