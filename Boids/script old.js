78df531ea5d1ea6bed54cd5bb96199f60eb159db
Loes Oldhoff <loesoldhoff@hotmail.com>
2023-03-16 17:05:55 +0100

    tried adding the 3 powers

--- Boids/script old.js
+++ Boids/script old.js
@@ -7,7 +7,7 @@ let centerx = canvas.width/2;
 let centery = canvas.height/2; 
 
 class Boid {
-    constructor(x, y, radius, angle, length, velocity, turnspeed, v1x, v1y, v2x, v2y){
+    constructor(x, y, radius, angle, length, velocity, turnspeed){
         this.x = x;
         this.y = y;
         this.radius = radius;
@@ -15,12 +15,7 @@ class Boid {
         this.length = length;
         this.velocity = velocity;
         this.turnspeed = turnspeed;
-        this.v1x = v1x;
-        this.v1y = v1y;
-        this.v2x = v2x;
-        this.v2y = v2y;
     }
-
     draw(){
         //draw head
         ctx.beginPath();
@@ -40,41 +35,17 @@ class Boid {
 
     }
     move(){
-        //Turn away from the edges of the canvas
-        //if(this.x < 0){
-        //    this.angle += this.turnspeed;}        
-        //if (this.x > canvas.width){
-        //    this.angle -= this.turnspeed;}
-        //if (this.y < 0){
-        //    this.angle += this.turnspeed;} 
-        //if (this.y > canvas.height){
-        //    this.angle -= this.turnspeed;}
+        if(this.x < 0){
+            this.angle += this.turnspeed;}        
+        if (this.x > canvas.width){
+            this.angle -= this.turnspeed;}
+        if (this.y < 0){
+            this.angle += this.turnspeed;} 
+        if (this.y > canvas.height){
+            this.angle -= this.turnspeed;}
 
-        //Calculate v1 (to center mass)
-        this.v1x = (centerx - this.x)/500;
-        this.v1y = (centery - this.y)/500;
-
-        //Calculate v2 (repulsion)
-        this.v2x = 0;
-        this.v2y = 0;
-        for (let i = 0; i < boids.length; i++){
-            let distance = Math.log(Math.pow(boids[i].x - this.x) + Math.pow(boids[i].y - this.y))
-            if (distance < 100 && distance != 0){
-                console.log('yolo')
-                this.v2x -= boids[i].x - this.x
-                this.v2y -= boids[i].y - this.y
-            }
-            else {
-                console.log('nah')
-            }             
-        }
-
-        //this.x = this.x+(this.velocity*Math.sin(this.angle)) + this.v2x;
-        //this.y = this.y+(this.velocity*Math.cos(this.angle)) + this.v2y;
-        //this.x += this.v1x + this.v2x;
-        //this.y += this.v1y + this.v2y;       
-        this.x += this.v2x; 
-        this.y += this.v2y;
+        this.x = this.x+(this.velocity*Math.sin(this.angle));
+        this.y = this.y+(this.velocity*Math.cos(this.angle));      
     }
 }
 
@@ -87,12 +58,6 @@ function drawcenter(){
     }
     x = x/boids.length;
     y = y/boids.length;
-    if (x > canvas.width){
-        x = canvas.width
-    }
-    if (y > canvas.height){
-        y = canvas.height
-    }
     ctx.beginPath();
     ctx.arc(x, y, 10, 0, Math.PI * 2, true);
     ctx.fillStyle = 'gold';
@@ -116,13 +81,11 @@ function init(){
             angle: Math.random(),           
             velocity: 1,
             turnspeed: 0.1,
-            v1x: 0,
-            v1y: 0, 
-            v2x: startx,
-            v2y: starty
+            v1x: startx,
+            v1y: starty
         };
-        boids.push(new Boid(boid.x, boid.y, boid.radius, boid.angle, boid.length, boid.velocity, 
-            boid.turnspeed, boid.v1x, boid.v1y, boid.v2x, boid.v2y));
+        boids.push(new Boid(boid.x, boid.y, boid.radius, boid.angle, boid.length, boid.velocity, boid.turnspeed,
+            boid.v1x, boid.v1y));
         counter += 1;
     }
 }
@@ -130,7 +93,6 @@ function init(){
 function animate(){
     requestAnimationFrame(animate);
     ctx.fillStyle = 'black';
-    console.log('hi')
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     for (let i = 0; i < boids.length; i++) {
         boids[i].draw();
